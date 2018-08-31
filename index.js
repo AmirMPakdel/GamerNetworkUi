ee31cfd8e6df50e95df573d910832381c0aa457e
Amir Reza <arg7971@gmail.com>
2018-08-31 12:48:42 +0430

    integrated redux and redux dev tools with react native debigger

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import navigationReducer from './src/store/reducers/navigationReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     navReducer: navigationReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeEnhancers());
 
 const Redux = () => (
     <Provider store={store}>
